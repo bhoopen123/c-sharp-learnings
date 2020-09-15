72b7de0301533a89c2f7ef095072562b76524e9b
Bhoopendra Kumar Sharma <jsharmbh@jci.com>
2020-09-16 02:24:26 +0530

    Generics code examples

---
+++ src/GenericsDemos/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace GenericsDemos
+{
+    class Program
+    {
+        /// <summary>
+        /// we can declare a generic method with a type parameter.
+        /// The following program illustrates the concept:
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="lhs"></param>
+        /// <param name="rhs"></param>
+        static void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T temp;
+            temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+
+        static void Main(string[] args)
+        {
+            // ***********************************************************************************
+            // To Call Generic Method...
+            // ***********************************************************************************
+            int a, b;
+            char c1, d1;
+            a = 10;
+            b = 20;
+            c1 = 'I';
+            d1 = 'V';
+
+            //display values before swap:
+            Console.WriteLine("Int values before calling swap:");
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+            Console.WriteLine("Char values before calling swap:");
+            Console.WriteLine("c = {0}, d = {1}", c1, d1);
+
+            //call swap // which is a generic method
+            Swap<int>(ref a, ref b);
+            Swap<char>(ref c1, ref d1);
+
+            //display values after swap:
+            Console.WriteLine("Int values after calling swap:");
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+            Console.WriteLine("Char values after calling swap:");
+            Console.WriteLine("c = {0}, d = {1}", c1, d1);
+            Console.ReadKey();
+
+
+            /// Generic Class Test 
+            //declaring an int array
+            MyGenericArray<int> intArray = new MyGenericArray<int>(5);
+            //setting values
+            for (int c = 0; c < 5; c++)
+            {
+                intArray.setItem(c, c * 5);
+            }
+            //retrieving the values
+            for (int c = 0; c < 5; c++)
+            {
+                Console.Write(intArray.getItem(c) + " ");
+            }
+            Console.WriteLine();
+            //declaring a character array
+            MyGenericArray<char> charArray = new MyGenericArray<char>(5);
+            //setting values
+            for (int c = 0; c < 5; c++)
+            {
+                charArray.setItem(c, (char)(c + 97));
+            }
+            //retrieving the values
+            for (int c = 0; c < 5; c++)
+            {
+                Console.Write(charArray.getItem(c) + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    /// <summary>
+    /// Generic Class
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class MyGenericArray<T>
+    {
+        private T[] array;
+        public MyGenericArray(int size)
+        {
+            array = new T[size + 1];
+        }
+        public T getItem(int index)
+        {
+            return array[index];
+        }
+        public void setItem(int index, T value)
+        {
+            array[index] = value;
+        }
+    }
+}
