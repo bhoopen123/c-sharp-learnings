4fc2ac7c46d880bb95d6b9f124d48203a020253f
Bhoopendra Sharma <bhoopendras73@gmail.com>
2025-05-13 22:54:37 +0530

    Thread Demos

---
+++ src/16_Threads_Demos/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+var mainThread = Thread.CurrentThread;
+Console.WriteLine($"Hello, World! on threadId : {mainThread.ManagedThreadId}, threadName : {mainThread.Name}");
