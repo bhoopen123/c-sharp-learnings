c86f21c056d7201c8af3bdfec273c81faa833a0d
Bhoopendra Sharma <bhoopendras73@gmail.com>
2025-05-26 00:09:37 +0530

    Threading : Adder Subtractor Problem

---
+++ src/16_Threads_Demos/Classes.cs
@@ -0,0 +1,50 @@
+﻿namespace _16_Threads_Demos
+{
+    public class Adder
+    {
+        Counter _counter;
+        public Adder(Counter counter)
+        {
+            _counter = counter;
+        }
+
+        public void Run()
+        {
+            for (int i = 0; i < 50000; i++)
+            {
+                _counter.Value += i;
+
+                //Console.WriteLine($"ThreadId : {Thread.CurrentThread.ManagedThreadId}, Hash : {_counter.GetHashCode()}, InitialValue : {_counter.Value}");
+                //Thread.Sleep(100); // Simulate some delay
+            }
+            //return initialValue.Value;
+        }
+    }
+
+    public class Subtractor
+    {
+        Counter _counter;
+
+        public Subtractor(Counter counter)
+        {
+            _counter = counter;
+        }
+
+        public void Run()
+        {
+            for (int i = 0; i < 50000; i++)
+            {
+                _counter.Value -= i;
+
+                //Console.WriteLine($"ThreadId : {Thread.CurrentThread.ManagedThreadId}, Hash : {_counter.GetHashCode()}, InitialValue : {_counter.Value}");
+                //Thread.Sleep(100); // Simulate some delay
+            }
+            //return _counter.Value;
+        }
+    }
+
+    public class Counter
+    {
+        public int Value { get; set; }
+    }
+}
--- src/16_Threads_Demos/Program.cs
+++ src/16_Threads_Demos/Program.cs
@@ -1,8 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using _16_Threads_Demos;
 using System.Threading;
 
-var mainThread = Thread.CurrentThread;
-Console.WriteLine($"Hello, World! on threadId : {mainThread.ManagedThreadId}, threadName : {mainThread.Name}");
+//var mainThread = Thread.CurrentThread;
+//Console.WriteLine($"Hello, World! on threadId : {mainThread.ManagedThreadId}, threadName : {mainThread.Name}");
 
 //Thread thread1 = new Thread(new ThreadStart(new DemoPrinter().Print));
 //thread1.Start();
@@ -10,33 +11,58 @@ Console.WriteLine($"Hello, World! on threadId : {mainThread.ManagedThreadId}, th
 //Thread thread2 = new Thread(new ThreadStart(new DemoPrinter().Print));
 //thread2.Start();
 
-for (int i = 0; i < 10; i++)
-{
-    Thread thread = new Thread(new ParameterizedThreadStart(new DemoPrinter().Print));
-    // Pass the values to the thread
-    thread.Start(i);
-}
+//for (int i = 0; i < 10; i++)
+//{
+//    Thread thread = new Thread(new ParameterizedThreadStart(new DemoPrinter().Print));
+//    // Pass the values to the thread
+//    thread.Start(i);
+//}
 
-var t1 = Task.Run(() => ReadValue(10));
-var t2 = Task.Run(() => ReadValue(20));
-var t3 = Task.Run(() => ReadValue(30));
+//var t1 = Task.Run(() => ReadValue(10));
+//var t2 = Task.Run(() => ReadValue(20));
+//var t3 = Task.Run(() => ReadValue(30));
 
-//Task.WaitAll(t1, t2, t3);
-// No need to use WaitAll , as we are using Result property
-Console.WriteLine("Sum: {0}", t1.Result + t2.Result + t3.Result);
+////Task.WaitAll(t1, t2, t3);
+//// No need to use WaitAll , as we are using Result property
+//Console.WriteLine("Sum: {0}", t1.Result + t2.Result + t3.Result);
 
 
-Console.WriteLine("Done");
+//Console.WriteLine("Done");
 
-static int ReadValue(int input)
-{
-    Console.WriteLine($"Hello, value = {input} from threadId : {Thread.CurrentThread.ManagedThreadId}, threadName : {Thread.CurrentThread.Name}");
+//static int ReadValue(int input)
+//{
+//    Console.WriteLine($"Hello, value = {input} from threadId : {Thread.CurrentThread.ManagedThreadId}, threadName : {Thread.CurrentThread.Name}");
 
-    Console.WriteLine($"Sleep ThreadId : {Thread.CurrentThread.ManagedThreadId}, threadName : {Thread.CurrentThread.Name}");
+//    Console.WriteLine($"Sleep ThreadId : {Thread.CurrentThread.ManagedThreadId}, threadName : {Thread.CurrentThread.Name}");
 
-    Thread.Sleep(1000);
-    return input;
-}
+//    Thread.Sleep(1000);
+//    return input;
+//}
+
+
+//  Adder and Subtractor implementation
+Console.WriteLine("Adder and Subtractor implementation");
+Counter counter = new Counter() { Value = 0 };
+
+Adder adder = new Adder(counter);
+Subtractor subtractor = new Subtractor(counter);
+
+Thread addThread = new Thread(new ThreadStart(adder.Run));
+Thread subThread = new Thread(new ThreadStart(subtractor.Run));
+
+addThread.Start();
+subThread.Start();
+
+// Wait for both threads to complete
+addThread.Join();
+subThread.Join();
+
+// even if, Thread.Join() was called.. counter.Value will print the Random value
+Console.WriteLine($"Counter value {counter.Value}");
+//Thread.Sleep(1000); // Wait for tasks to complete
+//Console.WriteLine($"Counter value {counter.Value}, ValueFromAdder {valueFromAdder.Result} ValueFromSubtrator {valueFromSubtrator.Result}");
+
+Console.WriteLine("Done Adder Subtractor");
 
 public class DemoPrinter
 {
