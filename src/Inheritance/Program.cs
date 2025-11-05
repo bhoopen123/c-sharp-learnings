014e9f42390447e26a4a32124302cf8662f45dd8
Bhoopendra Sharma <bhoopendras73@gmail.com>
2025-11-05 22:58:13 +0530

    Inheritance demo 1

--- src/Inheritance/Program.cs
+++ src/Inheritance/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Inheritance
 {
@@ -76,7 +73,7 @@ namespace Inheritance
         {
             ClassB a = new ClassC();
             a.Print();
-            
+
             Derived d1 = new Derived(20);
 
             //string myConnection = "";
@@ -93,6 +90,32 @@ namespace Inheritance
             //b1.FunBase();
 
             Console.Read();
+
+            // we create the obj of Drived Class
+            drived obj = new drived();
+            Console.ReadKey();
+        }
+    }
+
+
+    class Base
+    {
+        protected string Name;      // u cannot access protected data from object instance..
+
+        // if base class has only the parametrised constructor then we will have to 
+        // give the value of the parameter of the base class 
+        // by using the 'Base' Keyword in the Constructor of the Drived Class 
+        public Base(int i)
+        {
+            Console.WriteLine("Base Constructor Call");
+        }
+    }
+
+    class drived : Base
+    {
+        public drived() : base(5)   // we have to give the value of the paremeter of the base class 
+        {                         // using the base keyword in the constructor of the child class
+            Console.WriteLine("Drived Constructor Call");
         }
     }
 }
