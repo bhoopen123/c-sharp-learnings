652d7a0efaefa9f59724fd685231bbc4f066ae23
Bhoopendra Sharma <bhoopendras73@gmail.com>
2025-11-10 23:24:38 +0530

    try fixing .NET conversion issue

--- src/UseOfEnvironmentClass/Program.cs
+++ src/UseOfEnvironmentClass/Program.cs
@@ -6,25 +6,25 @@ namespace UseOfEnvironmentClass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Current OS : {0} ",Environment.OSVersion);
+            Console.WriteLine("Current OS : {0} ", Environment.OSVersion);
 
-            Console.WriteLine("Current Directory : {0}",Environment.CurrentDirectory);
+            Console.WriteLine("Current Directory : {0}", Environment.CurrentDirectory);
 
             string[] drives = Environment.GetLogicalDrives();
-            for (int i = 0; i < drives.Length;i++ )
+            for (int i = 0; i < drives.Length; i++)
             {
-                Console.WriteLine("Drive {0} : {1}",i,drives[i]);
+                Console.WriteLine("Drive {0} : {1}", i, drives[i]);
             }
 
-            Console.WriteLine("Executing Version of .NET: {0}",Environment.Version);
+            Console.WriteLine("Executing Version of .NET: {0}", Environment.Version);
 
-            Console.WriteLine("Machine Name: {0}",Environment.MachineName);
+            Console.WriteLine("Machine Name: {0}", Environment.MachineName);
 
-            Console.WriteLine("Processsor Count; {0}",Environment.ProcessorCount);
+            Console.WriteLine("Processsor Count; {0}", Environment.ProcessorCount);
 
-            Console.WriteLine("System Directory : {0}",Environment.SystemDirectory);
+            Console.WriteLine("System Directory : {0}", Environment.SystemDirectory);
 
-            Console.WriteLine("UserName : {0}",Environment.UserName);
+            Console.WriteLine("UserName : {0}", Environment.UserName);
 
             Console.ReadKey();
 
