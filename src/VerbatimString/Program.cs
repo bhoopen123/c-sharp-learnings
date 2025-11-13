eae8b31b21fc9af4ea5efcb634add5bf8baa5474
Bhoopendra Sharma <bhoopendras73@gmail.com>
2025-11-13 23:56:55 +0530

    String : Composite Formate Vs String Interpolation

--- src/VerbatimString/Program.cs
+++ src/VerbatimString/Program.cs
@@ -1,12 +1,33 @@
-﻿using System;
-
-namespace VerbatimString
+﻿namespace VerbatimString
 {
     class Program
     {
         static void Main(string[] args)
         {
+            #region String Composite Formatting Vs String Interpolation
 
+            // String Composite Formatting Vs String Interpolation
+            string Name = "John";
+            DateTime today = DateTime.Now;
+
+            // String Composite Formatting
+            Console.WriteLine("Hello {0}, Today is {1:dddd, MMMM dd, yyyy}", Name, today);
+
+            // String Interpolation
+            Console.WriteLine($"Hello {Name}, Today is {today:dddd, MMMM dd, yyyy}");
+
+            // An interpolated string starts with the '$' symbol before the opening double-quote ( " )
+
+            // An interpolated string can be used to initlized a Constant also, only if all the inpolated expressions within the interpolated strings are constant strings.
+            const string SomeName = "John";
+            const string Statement = $"Hello {SomeName}, Greetings";
+
+            #endregion
+
+
+            #region Verbatim Identifier
+
+            // "@" is known as verbatim identifier in c#
             // The Following string is printed verbatim
             // thus, all escape characters are displayed
             Console.WriteLine(@"c:\gjgj\hhjg\kkkjgjh");
@@ -22,6 +43,13 @@ aggafggfgf
             // you can also insert a double quote into a literal string by doubing the " token,
             Console.WriteLine(@"Cerebus said ""Darr! Pret-ty sun_sets""");
             Console.Read();
+
+            // another usages of "@"
+            // to use c# keyword as identifier
+            int @class = 10;
+            Console.WriteLine("Using c# keyword as identifier ", @class);
+
+            #endregion
         }
     }
 }
