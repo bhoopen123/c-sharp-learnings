e3089f5aad7caca827e3fc3808ede8abc90e4f0e
Bhoopendra Sharma <bhoopendras73@gmail.com>
2025-11-15 22:14:25 +0530

    code comments in Threads_Demo

--- src/16_Threads_Demos/Program.cs
+++ src/16_Threads_Demos/Program.cs
@@ -1,48 +1,55 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using _16_Threads_Demos;
 
-//var mainThread = Thread.CurrentThread;
-//Console.WriteLine($"Hello, World! on threadId : {mainThread.ManagedThreadId}, threadName : {mainThread.Name}");
+var mainThread = Thread.CurrentThread;
+Console.WriteLine($"Hello, World! on threadId : {mainThread.ManagedThreadId}, threadName : {mainThread.Name}");
 
-//Thread thread1 = new Thread(new ThreadStart(new DemoPrinter().Print));
-//thread1.Start();
+// Creating a thread using ThreadStart delegate which does not take any parameters and does not return any value
+Thread thread1 = new Thread(new ThreadStart(new DemoPrinter().Print));
+thread1.Name = "Thread1";
 
-//Thread thread2 = new Thread(new ThreadStart(new DemoPrinter().Print));
-//thread2.Start();
+Thread thread2 = new Thread(new ThreadStart(new DemoPrinter().Print));
+thread2.Name = "Thread2";
 
-//for (int i = 0; i < 10; i++)
-//{
-//    Thread thread = new Thread(new ParameterizedThreadStart(new DemoPrinter().Print));
-//    // Pass the values to the thread
-//    thread.Start(i);
-//}
+thread1.Start();
+thread2.Start();
 
-//var t1 = Task.Run(() => ReadValue(10));
-//var t2 = Task.Run(() => ReadValue(20));
-//var t3 = Task.Run(() => ReadValue(30));
+// Creating multiple threads in a loop
+for (int i = 0; i < 10; i++)
+{
+    // Creating a thread using ParameterizedThreadStart delegate which takes an object parameter and does not return any value
+    Thread thread = new Thread(new ParameterizedThreadStart(new DemoPrinter().Print));
+    thread.Name = $"Thread-{i}";
+    // Pass the values to the thread, when it starts
+    thread.Start(i);
+}
 
-////Task.WaitAll(t1, t2, t3);
-//// No need to use WaitAll , as we are using Result property
-//Console.WriteLine("Sum: {0}", t1.Result + t2.Result + t3.Result);
+var t1 = Task.Run(() => ReadValue(10));
+var t2 = Task.Run(() => ReadValue(20));
+var t3 = Task.Run(() => ReadValue(30));
 
-
-//Console.WriteLine("Done");
-
-//static int ReadValue(int input)
-//{
-//    Console.WriteLine($"Hello, value = {input} from threadId : {Thread.CurrentThread.ManagedThreadId}, threadName : {Thread.CurrentThread.Name}");
-//    Console.WriteLine($"Sleep ThreadId : {Thread.CurrentThread.ManagedThreadId}, threadName : {Thread.CurrentThread.Name}");
-//    Thread.Sleep(1000);
-//    return input;
-//}
+// Task.WaitAll(t1, t2, t3);
+// No need to use WaitAll , as we are using Result property
+Console.WriteLine("Sum: {0}", t1.Result + t2.Result + t3.Result);
 
 
-//  Adder and Subtractor implementation
+Console.WriteLine("Done");
+
+static int ReadValue(int input)
+{
+    Console.WriteLine($"Hello, value = {input} from threadId : {Thread.CurrentThread.ManagedThreadId}, threadName : {Thread.CurrentThread.Name}");
+    Console.WriteLine($"Sleep ThreadId : {Thread.CurrentThread.ManagedThreadId}, threadName : {Thread.CurrentThread.Name}");
+    Thread.Sleep(1000);
+    return input;
+}
+
+
+// Adder and Subtractor implementation
 Console.WriteLine("Adder and Subtractor implementation");
 Counter counter = new Counter() { Value = 0 };
 
 // Create a mutex to synchronize access to the counter
-Mutex mutex = new Mutex(false, "SyncLock"); 
+Mutex mutex = new Mutex(false, "SyncLock");
 
 Adder adder = new Adder(counter, mutex);
 Subtractor subtractor = new Subtractor(counter, mutex);
@@ -69,13 +76,13 @@ public class DemoPrinter
     public void Print()
     {
         var thread = Thread.CurrentThread;
-        Console.WriteLine($"Hello, World! on threadId : {thread.ManagedThreadId}, threadName : {thread.Name}");
+        Console.WriteLine($"On threadId : {thread.ManagedThreadId}, threadName : {thread.Name}");
     }
 
     public void Print(object? value)
     {
         int i = (int)value;
         var thread = Thread.CurrentThread;
-        Console.WriteLine($"print {i} on threadId : {thread.ManagedThreadId}, threadName : {thread.Name}");
+        Console.WriteLine($"Value {i} on threadId : {thread.ManagedThreadId}, threadName : {thread.Name}");
     }
 }
