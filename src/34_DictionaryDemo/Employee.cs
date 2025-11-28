2517f7d3f020f80c10ba9c8b841a2ea0e484395f
Bhoopendra Sharma <bhoopendras73@gmail.com>
2025-11-28 23:44:43 +0530

    recreated DictionaryDemo project

---
+++ src/34_DictionaryDemo/Employee.cs
@@ -0,0 +1,19 @@
+﻿namespace _34_DictionaryDemo
+{
+    internal class Employee : IEqualityComparer<Employee>
+    {
+        public string _name;
+
+        public Employee(string name)
+        {
+            _name = name;
+        }
+
+        public bool Equals(Employee x, Employee y) => x._name == y._name;
+
+        public int GetHashCode(Employee obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
